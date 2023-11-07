5a0164c355695ba8027e461ff02f42e2fc88c656
kwangsik Kim <penggusic@gmail.com>
2023-11-07 16:26:43 +0900

    ajax get 사용 state 업데이트. 연습

---
+++ src/main/java/com/example/spring20230920/controller/RestController1.java
@@ -0,0 +1,37 @@
+package com.example.spring20230920.controller;
+
+import com.example.spring20230920.dao.MyDao10;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("api/main1")
+public class RestController1 {
+
+    private final MyDao10 dao;
+
+    // http://localhost:8080/api/main1/sub1
+    @GetMapping("sub1")
+    @ResponseBody
+    public String method1() {
+        return "customer name!!";
+    }
+
+
+    // http://localhost:8080/api/main1/sub2
+    // get 요청시 50번 고객의 customerName 응답
+
+    @GetMapping("sub2")
+    @ResponseBody
+    public String method2() {
+        return dao.getCustomerName();
+    }
+}
+
---
+++ src/main/java/com/example/spring20230920/dao/MyDao10.java
@@ -0,0 +1,15 @@
+package com.example.spring20230920.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Map;
+
+@Mapper
+public interface MyDao10 {
+    @Select("""
+            SELECT CustomerName FROM customers
+            WHERE CustomerID = 50
+            """)
+    String getCustomerName();
+}
